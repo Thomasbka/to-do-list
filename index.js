5599f44105c170350dc3da54aab6eeefe0508f18
Thomasbka <tolle_bka@hotmail.com>
2024-04-29 13:23:21 +0200

    switched to using jQuery.

--- index.js
+++ index.js
@@ -1,65 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var todoList = document.getElementById('todo-list');
-  var todoInput = document.getElementById('todo-input');
-  var addButton = document.getElementById('add-button');
+$(document).ready(function() {
+  var todoList = $('#todo-list');
+  var todoInput = $('#todo-input');
+  var addButton = $('#add-button');
   var todoCount = 0;
 
   var addTodo = function() {
-    var todoCol = document.createElement('div');
-    todoCol.setAttribute('class', 'col-xs-12 todo');
-  
-    var todoRow = document.createElement('div');
-    todoRow.setAttribute('class', 'row');
+    var todoCol = $('<div>', { class: 'col-xs-12 todo' });
+    var todoRow = $('<div>', { class: 'row' });
+    var h5 = $('<h5>', { class: 'col-xs-8', text: todoInput.val() });
 
-    var h5 = document.createElement('h5');
-    h5.setAttribute('class', 'col-xs-8');
-    h5.innerHTML = todoInput.value;
+    var successButton = $('<button>', {
+      class: 'btn btn-success',
+      text: 'DONE'
+    }).click(function() {
+      var textElement = $(this).parent().find('h5');
+      textElement.toggleClass('strike');
+    });
 
-    var successButton = document.createElement('button');
-    successButton.setAttribute('class', 'btn btn-success');
-    successButton.innerHTML = "DONE";
-    successButton.onclick = function() {
-      var textElement = this.parentNode.querySelector('h5');
-      if (textElement) {
-        textElement.classList.toggle('strike');
-      } else {
-        console.log('Text eleent not found.');
-      }
-    };
-  
-    var removeButton = document.createElement('button');
-    removeButton.setAttribute('class','btn btn-danger remove-button');
-  
-    removeButton.innerHTML = "REMOVE";
+    var removeButton = $('<button>', {
+      class: 'btn btn-danger remove-button',
+      text: 'REMOVE'
+    }).click(function() {
+      $(this).closest('.col-xs-12.todo').remove();
+    });
 
-    removeButton.onclick = function() {
-      var child = this.parentNode.parentNode;
-
-      todoList.removeChild(child);
-    };
-  
-    todoRow.appendChild(h5);
-    todoRow.appendChild(successButton);
-    todoRow.appendChild(removeButton);
-    todoCol.appendChild(todoRow);
-    todoList.appendChild(todoCol);
+    todoRow.append(h5, successButton, removeButton);
+    todoCol.append(todoRow);
+    todoList.append(todoCol);
   };
-  
-  var checkThenAddTodo = function () {
 
-    if (todoCount < 10 && todoInput.value !== '') {
-  
+  var checkThenAddTodo = function() {
+    if (todoCount < 10 && todoInput.val() !== '') {
       addTodo();
-  
       todoCount++;
-  
-      todoInput.value = '';
+      todoInput.val('');
     }
   }
-  
-  addButton.addEventListener('click', checkThenAddTodo);
-  
-  todoInput.addEventListener('keyup', function (event) {
+
+  addButton.click(checkThenAddTodo);
+
+  todoInput.keyup(function(event) {
     if (event.key === "Enter") {
       checkThenAddTodo();
     }
