5500134bce294cc9ab1a45ab59901a26cafe2567
Thomasbka <tolle_bka@hotmail.com>
2024-05-02 12:53:18 +0200

    The index.js has been corrected so that it is connected with the ATDAPI service. Small changes in the html as well to make js work. Next step will be to clean it all up with some css.

--- index.js
+++ index.js
@@ -1,47 +1,100 @@
-$(document).ready(function() {
-  var todoList = $('#todo-list');
-  var todoInput = $('#todo-input');
-  var addButton = $('#add-button');
-  var todoCount = 0;
-
-  var addTodo = function() {
-    var todoCol = $('<div>', { class: 'col-xs-12 todo' });
-    var todoRow = $('<div>', { class: 'row' });
-    var h5 = $('<h5>', { class: 'col-xs-8', text: todoInput.val() });
-
-    var successButton = $('<button>', {
-      class: 'btn btn-success',
-      text: 'DONE'
-    }).click(function() {
-      var textElement = $(this).parent().find('h5');
-      textElement.toggleClass('strike');
+$(document).ready(function(){
+  
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1244',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+        });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
     });
+  }
+  
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1244',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
 
-    var removeButton = $('<button>', {
-      class: 'btn btn-danger remove-button',
-      text: 'REMOVE'
-    }).click(function() {
-      $(this).closest('.col-xs-12.todo').remove();
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=1244',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
     });
-
-    todoRow.append(h5, successButton, removeButton);
-    todoCol.append(todoRow);
-    todoList.append(todoCol);
-  };
-
-  var checkThenAddTodo = function() {
-    if (todoCount < 10 && todoInput.val() !== '') {
-      addTodo();
-      todoCount++;
-      todoInput.val('');
-    }
   }
 
-  addButton.click(checkThenAddTodo);
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
 
-  todoInput.keyup(function(event) {
-    if (event.key === "Enter") {
-      checkThenAddTodo();
+  var markTaskComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1244',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1244',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
     }
   });
+
+  getAndDisplayAllTasks();
 });
